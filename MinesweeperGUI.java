5d71c1c4e75811229851250c1c88875316e4c73c
Mitch Woollatt <21175347+MitchellAW@users.noreply.github.com>
2017-07-16 23:08:04 +0930

    Basic rewrite using MouseListener

--- MinesweeperGUI.java
+++ MinesweeperGUI.java
@@ -3,9 +3,6 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -25,7 +22,11 @@ public class MinesweeperGUI extends JFrame {
 
 	boolean gameOver = false;
 
-	Color bgColour = new Color(236,236,236);
+	String flag = Character.toString(board.flag);
+	String mine = Character.toString(board.mine);
+
+	Color background = new Color(236,236,236);
+	Color defaultBg = new JButton().getBackground();
 
 	public MinesweeperGUI() {
 		frame.setSize(600, 600);
@@ -35,7 +36,8 @@ public class MinesweeperGUI extends JFrame {
 	}
 	private void initialise() {
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		JPanel gameBoard = new JPanel(new GridLayout(board.getHeight(),board.getWidth()));		
+		JPanel gameBoard = new JPanel(new GridLayout(board.getHeight(),
+				board.getWidth()));		
 
 		frame.add(mainPanel);
 
@@ -44,7 +46,7 @@ public class MinesweeperGUI extends JFrame {
 		mainPanel.add(gameBoard, BorderLayout.NORTH);
 		mainPanel.add(resetButton);
 
-		resetButton.addActionListener(new myActionListener());
+		resetButton.addMouseListener(new mouseListener());
 
 		// Initialise all the buttons
 		for(int i=0; i<board.getHeight(); i++) {
@@ -54,157 +56,83 @@ public class MinesweeperGUI extends JFrame {
 				setColour(i, j);
 
 				gameBoard.add(boardButtons[i][j]); 
-				boardButtons[i][j].addActionListener(new myActionListener());
 				boardButtons[i][j].addMouseListener(new mouseListener());
 
 				// 30 for easy, 
-				boardButtons[i][j].setFont(new Font("Arial Unicode MS", Font.BOLD, 30));
+				boardButtons[i][j].setFont(new Font("Arial Unicode MS", 
+						Font.BOLD, 30));
 			}
 		}
 	}
-	// Listen for when the buttons are clicked
-	private class myActionListener implements ActionListener {
-		public void actionPerformed(ActionEvent action) {
-
-			//Display the player's piece on the buttons
-			if (gameOver == false) {
-				for(int i=0; i<board.getHeight(); i++) {
-					for (int j=0; j<board.getWidth(); j++) {
-						if (action.getSource() == boardButtons[i][j] && 
-								board.isPiece(i, j, board.getMine())) {
-							gameOver = true;
-							revealMines();
-							boardButtons[i][j].setBackground(Color.RED);
-						} else if (action.getSource() == boardButtons[i][j] &&
-								board.isPiece(i, j, ' ')) {
-							revealBlanks(i, j);
-							if (isWinner()) {
-								gameOver = true;
-								winner();
-							}
-						} else if (action.getSource() == boardButtons[i][j]) {
-							reveal(i, j);
-							if (isWinner()) {
-								gameOver = true;
-								winner();
-							}
-						}
-					}
-				}
-			}
-			// When the reset button is clicked, reset the board & the display
-			if(action.getSource() == resetButton) {
-				board.reset();
-				gameOver = false;
-				for(int i=0; i<board.getHeight(); i++) {
-					for (int j=0; j<board.getWidth(); j++) { 
-						boardButtons[i][j].setEnabled(true);
-						boardButtons[i][j].setBackground(new JButton().getBackground());
-						boardButtons[i][j].setForeground(Color.BLACK);
-						boardButtons[i][j].setText("");
-						setColour(i, j);
-						frame.setTitle("Mitch's Minesweeper");
-					}
-				}
-			}
-		}
-	}
-	public class mouseListener implements MouseListener {
-		@Override
-		public void mouseClicked(MouseEvent arg0) {
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent arg0) {
-		}
-
-		@Override
-		public void mouseExited(MouseEvent arg0) {
-		}
-
-		@Override
-		public void mousePressed(MouseEvent e) {
-			for (int i=0; i<boardButtons.length; i++) {
-				for (int j=0; j<boardButtons[0].length; j++) {
-					if (e.getButton() == 3 && e.getSource() == boardButtons[i][j]) {
-						setFlag(i, j);
-					}
-				}
-			}
-		}
-		@Override
-		public void mouseReleased(MouseEvent arg0) {
-		}
-
-	}
-	public void setColour(int row, int column) {
-		String currentPiece = board.getPieceAt(row, column);
+	public void setColour(int row, int col) {
+		String currentPiece = board.getPieceAt(row, col);
 		switch (currentPiece) {
 		case "1":
-			boardButtons[row][column].setForeground(Color.BLUE);
+			boardButtons[row][col].setForeground(Color.BLUE);
 			break;
 		case "2":
-			boardButtons[row][column].setForeground(Color.GREEN.darker());
+			boardButtons[row][col].setForeground(Color.GREEN.darker());
 			break;
 		case "3":
-			boardButtons[row][column].setForeground(Color.RED);
+			boardButtons[row][col].setForeground(Color.RED);
 			break;
 		case "4":
-			boardButtons[row][column].setForeground(Color.BLUE.darker());
+			boardButtons[row][col].setForeground(Color.BLUE.darker());
 			break;
 		case "5":
-			boardButtons[row][column].setForeground(Color.RED.darker());
+			boardButtons[row][col].setForeground(Color.RED.darker());
 			break;
 		case "6":
-			boardButtons[row][column].setForeground(Color.BLUE.brighter());
+			boardButtons[row][col].setForeground(Color.BLUE.brighter());
 			break;
 		case "7":
-			boardButtons[row][column].setForeground(Color.BLACK);
+			boardButtons[row][col].setForeground(Color.BLACK);
 			break;
 		case "8":
-			boardButtons[row][column].setForeground(Color.GRAY.brighter());
+			boardButtons[row][col].setForeground(Color.GRAY.brighter());
 			break;	
 		}
 	}
-	public void reveal(int row, int column) {
-		boardButtons[row][column].setText(board.getPieceAt(row, column));
-		boardButtons[row][column].setBackground(bgColour);
-		setColour(row, column);
+	public void reveal(int row, int col) {
+		if (board.isPiece(row, col, ' ')) {
+			revealBlanks(row, col);
+		} else {
+			boardButtons[row][col].setText(board.getPieceAt(row, col));
+			boardButtons[row][col].setBackground(background);
+			setColour(row, col);
+		}
 	}
 	public void revealMines() {
 		for (int i=0; i<boardButtons.length; i++) {
 			for (int j=0; j<boardButtons[0].length; j++) {
-				if (board.isPiece(i, j, board.getMine())) {
+				if (board.isPiece(i, j, board.mine)) {
 					boardButtons[i][j].setForeground(Color.BLACK);
 					boardButtons[i][j].setText(board.getPieceAt(i, j));
 				}
 			}
 		}
 	}
-	public void revealBlanks(int row, int column) {
-		if (!boardButtons[row][column].getBackground().equals(new JButton().getBackground())) {
+	public void revealBlanks(int row, int col) {
+		if (!boardButtons[row][col].getBackground().equals(defaultBg)) {
 			return;
 		}
-		if (board.isPiece(row, column, ' ')) {
-			boardButtons[row][column].setBackground(bgColour);
-			if (board.isPiece(row - 1, column, ' ')) { revealBlanks(row-1, column);}
-			if (board.isPiece(row - 1, column + 1, ' ')) { revealBlanks(row-1, column+1);}
-			if (board.isPiece(row, column + 1, ' ')) { revealBlanks(row, column+1);}
-			if (board.isPiece(row + 1, column + 1, ' ')) { revealBlanks(row+1, column+1);}
-			if (board.isPiece(row + 1, column, ' ')) { revealBlanks(row+1, column);}
-			if (board.isPiece(row + 1, column - 1, ' ')) { revealBlanks(row+1, column-1);}
-			if (board.isPiece(row, column - 1, ' ')) { revealBlanks(row, column-1);}
-			if (board.isPiece(row - 1, column - 1, ' ')) { revealBlanks(row-1, column-1);}
+		if (board.isPiece(row, col, ' ')) {
+			boardButtons[row][col].setBackground(background);
+			if (board.isPiece(row - 1, col, ' ')) { revealBlanks(row-1, col);}
+			if (board.isPiece(row - 1, col + 1, ' ')) { revealBlanks(row-1, col+1);}
+			if (board.isPiece(row, col + 1, ' ')) { revealBlanks(row, col+1);}
+			if (board.isPiece(row + 1, col + 1, ' ')) { revealBlanks(row+1, col+1);}
+			if (board.isPiece(row + 1, col, ' ')) { revealBlanks(row+1, col);}
+			if (board.isPiece(row + 1, col - 1, ' ')) { revealBlanks(row+1, col-1);}
+			if (board.isPiece(row, col - 1, ' ')) { revealBlanks(row, col-1);}
+			if (board.isPiece(row - 1, col - 1, ' ')) { revealBlanks(row-1, col-1);}
 		}
-	}
-	public static void main(String[] args) {
-		MinesweeperGUI game = new MinesweeperGUI();
-		game.initialise();
 	}
 	public boolean isWinner() {
 		for (int i=0; i<boardButtons.length; i++) {
 			for (int j=0; j<boardButtons[0].length; j++) {
-				if (!board.isPiece(i, j, board.getMine()) && boardButtons[i][j].getBackground().equals(new JButton().getBackground())) {
+				if (!board.isPiece(i, j, board.mine) && 
+						boardButtons[i][j].getBackground().equals(defaultBg)) {
 					return false;
 				}
 			}
@@ -214,12 +142,79 @@ public class MinesweeperGUI extends JFrame {
 	public void winner() {
 		frame.setTitle("Mitch's Minesweeper: ***** YOU WIN *****");
 	}
-	public void setFlag(int row, int column) {
-		if (!boardButtons[row][column].getText().equals(Character.toString(board.getFlag()))) {
-			boardButtons[row][column].setText(Character.toString(board.getFlag()));
-			boardButtons[row][column].setForeground(Color.RED);
+	public void setFlag(int row, int col) {
+		if (!boardButtons[row][col].getText().equals(flag)) {
+			boardButtons[row][col].setText(flag);
+			boardButtons[row][col].setForeground(Color.RED);
 		} else {
-			boardButtons[row][column].setText("");
+			boardButtons[row][col].setText("");
 		}
+	}
+	// When buttons are clicked
+	public class mouseListener implements MouseListener {
+		// Playing on left clicks
+		public void mouseClicked(MouseEvent e) {
+			if (gameOver == false && e.getButton() != 3) {
+				for(int i=0; i<board.getHeight(); i++) {
+					for (int j=0; j<board.getWidth(); j++) {
+						if(e.getSource() == boardButtons[i][j]
+								&&!boardButtons[i][j].getText().equals(flag)) {
+
+							// If a mine is clicked, reveal them
+							if (board.isPiece(i, j, board.mine)) {
+								gameOver = true;
+								revealMines();
+								boardButtons[i][j].setBackground(Color.RED);
+								// If piece was blank, reveal rest of blanks
+							} else {
+								reveal(i, j);
+								if (isWinner()) {
+									gameOver = true;
+									winner();
+								}
+							}
+						}
+					}
+				}
+			}
+			if(e.getSource() == resetButton) {
+				board.reset();
+				gameOver = false;
+				for(int i=0; i<board.getHeight(); i++) {
+					for (int j=0; j<board.getWidth(); j++) { 
+						boardButtons[i][j].setBackground(defaultBg);
+						boardButtons[i][j].setText("");
+						frame.setTitle("Mitch's Minesweeper");
+					}
+				}
+			}
+		}
+		// Flagging on right click press
+		public void mousePressed(MouseEvent e) {
+			if (gameOver == false) {
+				for (int i=0; i<boardButtons.length; i++) {
+					for (int j=0; j<boardButtons[0].length; j++) {
+						String buttonText = boardButtons[i][j].getText();
+						if (e.getButton() == 3 && 
+								e.getSource() == boardButtons[i][j]) {
+							// Flag if not already flagged
+							if (buttonText.equals("")) {
+								setFlag(i, j);
+								// Remove flag if already flagged
+							} else if (buttonText.equals(flag)) {
+								setFlag(i, j);
+							}
+						}
+					}
+				}
+			}
+		}
+		public void mouseEntered(MouseEvent arg0) { }
+		public void mouseExited(MouseEvent arg0) { }
+		public void mouseReleased(MouseEvent arg0) { }
+	}
+	public static void main(String[] args) {
+		MinesweeperGUI game = new MinesweeperGUI();
+		game.initialise();
 	}
 }
